9509cfa2885602124b653ba0da34a92756dd6f82
habibur nabi <habiburnabiarafat@gmail.com>
2024-03-21 02:18:53 +0600

    added gallery section

--- src/pages/Home/GalleryDonation/GalleryDonation.tsx
+++ src/pages/Home/GalleryDonation/GalleryDonation.tsx
@@ -1,10 +1,21 @@
-import PhotoAlbum from "react-photo-album"
-import photos from "./photos"
+
+import { PhotoAlbum } from "react-photo-album";
+import { photos } from "./photos";
+import { Typography } from "antd";
+
+const { Title } = Typography;
 
 const GalleryDonation = () => {
     return (
-        <PhotoAlbum photos={photos} layout="rows" />
+        <div style={{ width: '90vw', maxWidth: '1200px', margin: '40px auto', padding: '40px 0' }}>
+            <Title level={1} style={{ textAlign: 'center', paddingBottom: '30px' }}>Gallery</Title>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <div style={{ width: '100%', maxWidth: '800px' }}>
+                    <PhotoAlbum photos={photos} layout="masonry" />
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default GalleryDonation
+export default GalleryDonation;
--- src/pages/Home/GalleryDonation/photos.ts
+++ src/pages/Home/GalleryDonation/photos.ts
@@ -1,43 +1,117 @@
-import axios from "axios";
-
-const breakpoints = [1080, 640, 384, 256, 128, 96, 64, 48];
-
-const unsplashLink = (id: string, width: number, height: number) =>
-	`https://source.unsplash.com/${id}/${width}x${height}`;
-
-const fetchRandomPhotos = async (query: string, count: number) => {
-	try {
-		const response = await axios.get(
-			`https://api.unsplash.com/photos/random?query=${query}&count=${count}&client_id=z_hyVjLrt5pu4RxwOrTiTFL2SPUhH1PytFAAK41LDsg`
-		);
-		const unsplashPhotos = response.data;
-
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const photos = unsplashPhotos.map((photo: any) => ({
-			src: unsplashLink(photo.id, photo.width, photo.height),
-			width: photo.width,
-			height: photo.height,
-			srcSet: breakpoints.map((breakpoint: number) => {
-				const height = Math.round((photo.height / photo.width) * breakpoint);
-				return {
-					src: unsplashLink(photo.id, breakpoint, height),
-					width: breakpoint,
-					height,
-				};
-			}),
-		}));
-
-		return photos;
-	} catch (error) {
-		console.error("Error fetching random photos:", error);
-		return [];
-	}
-};
-
-const query =
-	"community helping, Post-Disaster,Community Health and Medical Supply"; // Add your desired keywords here
-const count = 10; // Number of random photos to fetch
-
-const photos = fetchRandomPhotos(query, count);
-
-export default photos;
+export const photos = [
+	{
+		src: "https://i.ibb.co/VT8mb67/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/0ydC66p/image.png",
+		width: 1,
+		height: 1,
+	},
+	{
+		src: "https://i.ibb.co/y50MrKV/image.png",
+		width: 3,
+		height: 4,
+	},
+	{
+		src: "https://i.ibb.co/7rNbbsk/image.png",
+		width: 3,
+		height: 4,
+	},
+	{
+		src: "https://i.ibb.co/0MBWy15/image.png",
+		width: 3,
+		height: 4,
+	},
+	{
+		src: "https://i.ibb.co/9r96pM6/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/YX3pgzL/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/y8pSPts/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/vz05LBy/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/ZTqdFpY/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/nDLcwsc/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/j5q8yLX/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/PTZQ4qB/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/KqSDdkz/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/P6jLb3n/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/r6FBx9f/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/rHgHqxN/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/xgvdfK5/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/fv46CTt/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/64FyrWn/Diana-Kabuswe-Makululu-Health-Facility-Maternal-Child-Health-Coordinator-showing-a-mom-about-correct.jpg",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/W6pcT6F/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/xLZ1KmB/image.png",
+		width: 4,
+		height: 3,
+	},
+	{
+		src: "https://i.ibb.co/vPBfJF4/image.png",
+		width: 4,
+		height: 3,
+	},
+];
